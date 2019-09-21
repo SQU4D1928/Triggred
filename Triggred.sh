8ef40d0e4454f907d92019020f18e2b665fc8798
SQU4D1928 <55623446+SQU4D1928@users.noreply.github.com>
2019-09-21 21:02:56 +0700

    Add files via upload

---
+++ Triggred.sh
@@ -0,0 +1,82 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay TOOLS
+toilet -f big -F gay TRIGGRED
+echo $green"=====================================
+AUTHOR : ARIEL DAN APRIYANI
+YOUTUBE : AG OFFICIAL
+===================================="
+toilet -f big -F gay Maaf
+toilet -f big -F gay Tools
+toilet -f big -F gay Cuma 3
+echo
+sleep 2
+echo $yellow"Silahkan Pilih Nomer Berapa :"
+echo
+echo $yellow"Nomer 2 Username Nya ridwan
+password nya erinafahira"
+echo
+echo $cyan"1.) LITEDDOS"
+echo
+echo $red"2.) PIPHISH"
+echo
+echo $green"3.) SQU4D"
+echo
+echo $blue
+read -p "Ariel Ganteng Kan?
+pilih Nomer Berapa Lu? : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay Ariel
+toilet -f big -F gay Love
+toilet -f big -F gay Apriyani
+echo $green"Ariel Sayang Apriyani"
+sleep 2
+git clone https://github.com/4L13199/LITEDDOS
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f big -F gay I Love
+toilet -f big -F gay You
+toilet -f big -F gay Apriyani
+echo $green"I Love You Apriyani"
+sleep 1
+git clone https://github.com/ridwanirawan/PiPhish
+cd PiPhish
+sh run.sh
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+toilet -f big -F gay Baruna
+toilet -f big -F gay 1928
+toilet -f big -F gay SQU4D
+toilet -f big -F gay 1928
+echo $yellow"BUCIN DULU EAAAAAAAAAA"
+sleep 1
+toilet -f big -F gay GUA
+toilet -f big -F gay Sayang
+toilet -f big -F gay Apriyani
+toilet -f big -F gay Ariel
+toilet -f big -F gay Gaming
+toilet -f big -F gay Official
+fi
+
+if [ $bro = 0 ] || [$bro = 0 ]
+then
+clear
+exit
+if
